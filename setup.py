369a86b93b294db33747bc89ec4e3b0cfb62917c
ReddTea <anibalrojas@outlook.com>
2023-01-18 00:43:03 -0300

    pip setup

--- setup.py
+++ setup.py
@@ -8,13 +8,12 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="reddemcee",
-    version="0.3.0",
+    version="0.3.3",
     author="ReddTea",
     author_email="redd@tea.com",
     description="An Adaptative Parallel Tempering wrapper for emcee 3 for personal use",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    #url="https://github.com/pypa/sampleproject",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -22,7 +21,6 @@ setuptools.setup(
     ],
     packages=setuptools.find_packages(where="src"),
     package_dir={"": "src"},
-    #data_files=[(path1, [path1+'/exo_list.csv', path1+'/ss_list.csv'])],
     include_package_data=True,
     install_requires=['numpy', 'matplotlib>=3.5.1', 'emcee', 'tqdm', 'pandas'],
     python_requires=">=3.6",
--- tests/test_reddemcee.py
+++ tests/test_reddemcee.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import scipy as sp
-
 try:
     pass
 except ImportError:
